f1eb037553aeffcadb1b776d096ff10b8ccf3ff0
Lloyd Konneker <bootch@nc.rr.com>
2013-12-10 06:03:20 -0500

    GUI changes

--- plugin-mycelium.py
+++ plugin-mycelium.py
@@ -8,8 +8,9 @@ For dev in Eclipse: in proj properties>PyDev PYTHONPATH>External libraries, add:
 from gimpfu import *
 
 
-def pluginMain(image, drawable, startPattern, maxPopulation, terminationPercent, squirminess, greedy,
-               exhaustion, mealCalories, burnCalories, reservesToDivide, renderToGray ):
+def pluginMain(image, drawable, startPattern, maxPopulation, terminationPercent, renderToGray, 
+               squirminess, greedy, exhaustion, 
+               mealCalories, burnCalories, reservesToDivide ):
   ''' 
   Glue to code in a Python package in same directory as this. 
   (Keep plugin code separate from this wrapper code.
@@ -22,28 +23,32 @@ def pluginMain(image, drawable, startPattern, maxPopulation, terminationPercent,
 
 register(
         "python_fu_mycelium",
-        "Render grayscale using worm automata.",
-        "Make a new image",
+        "Make new image rendered by moving automata, feeding on the original.",
+        "Make new image rendered by moving automata, feeding on the original.",
         "Lloyd Konneker",
         "Lloyd Konneker",
         "2013",
-        "<Image>/Filters/Render/Mycelium...",
+        "<Image>/Filters/Map/Mycelium...",
         "RGB*, GRAY*, INDEXED*",
         [
-          (PF_RADIO, "startPattern", "Starting field:", 1, (("Centered", 0), ("Uniform", 1))), 
-          (PF_SPINNER, "maxPopulation", "Max population:", 100, (1, 1000, 10)),
-          (PF_SLIDER, "terminationPercent", "Ending (percent):", 60, (1, 100, 10)),
-          (PF_RADIO, "squirminess", "Squirminess:", 0, (("Relaxed", 0), ("Curly", 1), ("Kinky", 2))),
-          (PF_TOGGLE, "greedy",   "Greedy:", 0),
-          (PF_RADIO, "exhaustion", "Exhaustion:", 1, (("Perish", 0), ("Migrate", 1), )),
-          (PF_SLIDER, "mealCalories", "Meal calories:", 26, (1, 255, 10)),
-          (PF_SLIDER, "burnCalories", "Daily burn:", 24, (1, 255, 10)),
-          (PF_SLIDER, "reservesToDivide", "Reserves to procreate:", 13, (1, 255, 10)),
-          (PF_TOGGLE, "renderToGray",   "Render mode gray:", 1),
-          
+          (PF_OPTION, "startPattern", "Starting field:", 1, ["Centered", "Uniform"]), 
+          (PF_SPINNER, "maxPopulation", "Max population:", 100, (1, 10000, 10)),
+          (PF_SLIDER, "terminationPercent", "Ending percent:", 60, (1, 100, 10)),
+          (PF_TOGGLE, "renderToGray",   "Mode to gray:", 1),
+          (PF_OPTION, "squirminess","Myce squirm:", 0, ["Relaxed","Curly","Kinky"]),
+          (PF_TOGGLE, "greedy",   "Myce greedy:", 0),
+          (PF_OPTION, "exhaustion","Myce exhausted:", 1, ["Die","Migrate"]),
+          (PF_SLIDER, "mealCalories", "Myce max daily intake:", 26, (1, 255, 10)),
+          (PF_SLIDER, "burnCalories", "Myce daily burn:", 24, (1, 255, 10)),
+          (PF_SLIDER, "reservesToDivide", "Myce divide on reserves of:", 13, (1, 255, 10)),
         ],
         [],
         pluginMain)
+
+#(PF_RADIO, "startPattern", "Starting field:", 1, (("Centered", 0), ("Uniform", 1))), 
+#(PF_RADIO, "exhaustion", "Exhaustion:", 1, (("Perish", 0), ("Migrate", 1), )),
+#(PF_RADIO, "squirminess", "Squirminess:", 0, (("Relaxed", 0), ("Curly", 1), ("Kinky", 2))),
+
 
 # Testing main
 ##def main():
